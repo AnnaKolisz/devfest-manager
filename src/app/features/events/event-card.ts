99ace884c91c288601ea6f595bc85a639dda2731
AnnaKolisz <annakolisz@10g.pl>
2026-03-02 15:19:18 +0100

    save first changes

--- src/app/features/events/event-card.ts
+++ src/app/features/events/event-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-event-card',
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
     <div
       class="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"
     >
-      <!-- TODO: Add Image -->
+<div class="relative h-48 w-full bg-gray-200">
+  <img
+    [src]="image()"
+    class="object-cover w-full h-full max-h-full max-w-full"
+    alt="Event thumbnail"
+  />
+</div>
 
       <div class="p-6">
         <div class="flex justify-between items-center mt-4">
@@ -14,13 +20,23 @@ import { Component } from '@angular/core';
           <p class="text-sm text-blue-600 font-semibold mb-2">TBA</p>
 
           <!-- TODO Mod 1: Add daysUntil() using @let -->
+           @let days = daysUntil();
+           @if(days !== null) {
           <div
             class="bg-blue-100 text-blue-800 text-xs font-bold px-3 py-1 rounded-full shadow-sm"
           ></div>
+          @if(days > 0) {
+            <span> In {{ days }} days</span>
+          } @else if(days < 0) {
+            <span >Past event</span>
+          } @else {
+        Happening Now!
+      }
+           }
         </div>
 
         <!-- TODO Mod 1: Add Title Input -->
-        <h3 class="text-xl font-bold text-gray-800 my-2">Title Placeholder</h3>
+        <h3 class="text-xl font-bold text-gray-800 my-2">{{ title() }}</h3>
 
         <div class="flex justify-between items-center mt-4">
           <!-- TODO Mod 1: Add Derived State (Like Button) -->
@@ -40,5 +56,17 @@ import { Component } from '@angular/core';
   `,
 })
 export class EventCard {
-  // TODO Mod 1: Implement input(), output(), and model()
+  title = input.required<string>();
+  image = input.required<string>();
+  date = input<string>();
+
+  daysUntil = computed(() => {
+    const eventDate = this.date();
+    if (!eventDate) return null;
+
+    const now = new Date();
+    const target = new Date(eventDate);
+    const diffTime = target.getTime() - now.getTime();
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  });
 }
